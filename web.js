97e2843a648706ddf284ab7729bda73954506ff2
Gayan Neomal Lorenzu Hewa <gayan@mint>
2013-07-15 04:12:24 +0530

    as

--- web.js
+++ web.js
@@ -2,11 +2,21 @@ var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var html;
+
+// Read HTML into memory.
+fs.readFile(__dirname + '/index.html', 'UTF-8', function (err, data) {
+    if (err) {
+	throw err;
+    }
+    html = data;
+});
 
 app.get('/', function(request, response) {
-    fs.readFile('index.html',function(err,data){
-	response.send(data);
-    });    
+    response.writeHead(200, {
+	'Content-Type': 'text/html'
+    });
+    response.send(data);
 });
 
 var port = process.env.PORT || 5000;
